90dfa9469640f5fa6c6bf4b8d9ad762fbc75e418
Matt Wyatt <mswyatt.td@gmail.com>
2024-02-12 16:19:43 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,81 @@
+const url = "https://api.thecatapi.com/v1/images/search";
+const api_key =
+  "live_giPVs1sO5rpWpBhFrYIEKGymNemqy6fMJXLJnvd9kMO3tRINAeIVXJvgSBIh8S0f";
+let userInput = document.getElementById("userInput");
+const container = document.getElementById("container");
+
+button.addEventListener("click", () => {
+  container.innerHTML = "";
+  console.log(userInput.value);
+  getImages(userInput.value);
+});
+
+// async function getImage() {
+//   try {
+//     const response = await fetch(url, {
+//       headers: {
+//         "x-api-key": api_key,
+//       },
+//     });
+//     const json = await response.json();
+//     createAndAppend(json[0].url);
+//     // let pic = document.createElement("img");
+//     // pic.src = `${json[0].url}`;
+//     // document.getElementById("container").appendChild(pic);
+//   } catch (err) {
+//     console.log(err);
+//   }
+// }
+
+function createAndAppend(imgURL) {
+  let picContainer = document.createElement("div");
+  document.getElementById("container").appendChild(picContainer);
+
+  let pic = document.createElement("img");
+  picContainer.appendChild(pic);
+
+  let upvoteButton = document.createElement("button");
+  upvoteButton.innerHTML = "upvote";
+  picContainer.appendChild(upvoteButton);
+
+  upvoteButton.addEventListener("click", () => {
+    upvote();
+  });
+
+  pic.src = imgURL;
+}
+
+async function getImages(val) {
+  let multiUrl = `https://api.thecatapi.com/v1/images/search?limit=${val}`;
+  try {
+    const response = await fetch(multiUrl, {
+      headers: {
+        "x-api-key": api_key,
+      },
+    });
+    let json = await response.json();
+    json = json.slice(0, parseInt(val));
+    for (picture of json) {
+      createAndAppend(picture.url);
+    }
+    // createAndAppend(json[0].url);
+    // let pic = document.createElement("img");
+    // pic.src = `${json[0].url}`;
+    // document.getElementById("container").appendChild(pic);
+  } catch (err) {
+    console.log(err);
+  }
+}
+
+async function upvote(obj) {
+  const response = await fetch(`https://api.thecatapi.com/v1/votes`, {
+    method: "POST",
+    headers: {
+      "x-api-key": api_key,
+    },
+    body: {
+      image_id: obj.id,
+      value: 1,
+    },
+  });
+}
